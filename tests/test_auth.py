5e8dfb680821bc8e130e15c0dba1f2b84817492d
Евгений <eldardzarahov504@gmail.com>
2026-03-21 20:16:46 +0300

    автотесты логина

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -15,6 +15,30 @@ def register_user(username, password):
 
     return response, response_body
 
+def login_user(username,password):
+    payload = {
+        "username": username,
+        "password": password
+    }
+    
+    response = requests.post(f"{BASE_URL}/login", json=payload)
+    response_body = response.json()
+
+    return response, response_body
+
+def test_successful_login():
+    unique_suffix = uuid.uuid4().hex[:6]
+    username = f"autotest_{unique_suffix}"
+    password = "Password123"
+
+    register_response, register_response_body = register_user(username, password)
+    login_response, login_response_body = login_user(username, password)
+
+    assert register_response.status_code == 201
+    assert register_response_body["message"] == "User created successfully"
+
+    assert login_response.status_code == 200
+    assert "access_token" in login_response_body
 
 def test_successful_registration():
     unique_suffix = uuid.uuid4().hex[:7]
@@ -24,7 +48,6 @@ def test_successful_registration():
 
     assert response.status_code == 201
     assert response_body["message"] == "User created successfully"
-
 
 def test_successful_registration_with_min_username_length():
     unique_suffix = uuid.uuid4().hex[:2]
@@ -74,3 +97,19 @@ def test_registration_with_invalid_password_returns_400():
 
     assert response.status_code == 400
     assert response_body["message"] == "Password must be from 8 to 32 characters"
+    
+
+def test_registration_with_existing_username_returns_409():
+    unique_suffix = uuid.uuid4().hex[:6]
+    username = f"autotest_{unique_suffix}"
+    password = "Password123"
+
+    first_response, first_response_body = register_user(username, password)
+    second_response, second_response_body = register_user(username, password)
+
+    assert first_response.status_code == 201
+    assert first_response_body["message"] == "User created successfully"
+
+    assert second_response.status_code == 409
+    assert second_response_body["message"] == "Username already exists"
+    

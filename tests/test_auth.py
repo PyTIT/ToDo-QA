284dea3c71a144b1a672368704cd988e341e6f91
Евгений <eldardzarahov504@gmail.com>
2026-03-24 20:25:42 +0300

    добавил автотест

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -26,18 +26,16 @@ def login_user(username,password):
 
     return response, response_body
 
-def create_task(title,description,priority, headers):
+def create_task(title, description, priority, headers):
     payload = {
         "title": title,
         "description": description,
         "priority": priority
     }
-    
+
     response = requests.post(f"{BASE_URL}/tasks", json=payload, headers=headers)
     response_body = response.json()
-    headers = {
-        "Authorization": f"Bearer {token}"
-    }
+
     return response, response_body
 
 
@@ -185,7 +183,7 @@ def test_create_task():
     login_response, login_response_body = login_user(username, password)
     
     payload = {
-        "title": "Test",
+        "title": "Тестовый заголовок",
         "description": "description",
         "priority": "high"
     }
@@ -237,3 +235,66 @@ def test_create_task_witout_title_returns_400():
     assert "access_token" in login_response_body
     
     assert response.status_code == 400
+
+def test_create_task_title_only_spaces_returns_400():
+    unique_suffix = uuid.uuid4().hex[:6]
+    username = f"autotest_{unique_suffix}"
+    password = "Password123"
+
+    register_response, register_response_body = register_user(username, password)
+    login_response, login_response_body = login_user(username, password)
+    
+    payload = {
+        "title": "   ",
+        "description": "description",
+        "priority": "high"
+    }
+
+    token = login_response_body["access_token"]
+    headers = {
+        "Authorization": f"Bearer {token}"
+    }
+    
+    response = requests.post(f"{BASE_URL}/tasks", headers=headers, json=payload)
+
+    assert register_response.status_code == 201
+    assert register_response_body["message"] == "User created successfully"
+
+    assert login_response.status_code == 200
+    assert "access_token" in login_response_body
+    
+    assert response.status_code == 400
+    
+def test_get_only_my_tasks():
+    unique_suffix = uuid.uuid4().hex[:6]
+    username = f"autotest_{unique_suffix}"
+    password = "Password123"
+
+    register_response, register_response_body = register_user(username, password)
+    login_response, login_response_body = login_user(username, password)
+    
+    payload = {
+        "title": "test title",
+        "description": "description",
+        "priority": "high"
+    }
+
+    token = login_response_body["access_token"]
+    headers = {
+        "Authorization": f"Bearer {token}"
+    }
+    
+    first_response = requests.post(f"{BASE_URL}/tasks", headers=headers, json=payload)
+    
+    second_response = requests.get(f"{BASE_URL}/tasks", headers=headers)
+
+    assert register_response.status_code == 201
+    assert register_response_body["message"] == "User created successfully"
+
+    assert login_response.status_code == 200
+    assert "access_token" in login_response_body
+    
+    assert first_response.status_code == 201
+    
+    assert second_response.status_code == 200
+    
